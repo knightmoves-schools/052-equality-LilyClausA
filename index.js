2420a6f168bc7f35ff5f4f1af4ff1427ca2bdc94
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-02-09 20:16:10 +0000

    only the first

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-let priceCheck = function(price){
-    return if === 1.50
+let priceCheck = function('price'){
+    if ('price') === 1.50;
+    return ('string match');
 }
 
 
 
 
 
-
+ 
 
 
 //should create a function named priceCheck 
@@ -14,17 +15,23 @@ let priceCheck = function(price){
 // string match if the price is equivalent 
 // to 1.50
 
-//should return match when the string 1.50 is passed in as an argument to
+//should return match when the string 1.50
+//  is passed in as an argument to
 //  priceCheck
 
-//should not return match when 1.00 is passed in as an argument to priceCheck
+//should not return match when 1.00 is
+//  passed in as an argument to priceCheck
 
-//should create a function named strictPriceCheck that takes a price and returns
-//  the string match only if the price is a number that is strictly equivalent 
-// to 1.50
+//should create a function named 
+// strictPriceCheck that takes a price and 
+// returns the string match only if the 
+// price is a number that is strictly 
+// equivalent to 1.50
 
-//should not return match when the string 1.50 is passed in as an argument to
+//should not return match when the string 
+// 1.50 is passed in as an argument to
 //  strictPriceCheck
 
-//should not return match when 1.00 is passed in as an argument to 
+//should not return match when 1.00 is 
+// passed in as an argument to 
 // strictPriceCheck
