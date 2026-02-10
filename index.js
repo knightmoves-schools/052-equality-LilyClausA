4716587e176b5a11354358054f5e4abd8fc60ad6
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-02-10 20:25:00 +0000

    h gybfcg h

--- index.js
+++ index.js
@@ -4,74 +4,15 @@ function priceCheck(price){
     }
 }
 
+function strictPriceCheck(price){
+    if (price === 1.50){
+        return "match";
+    }
+
+}
 
 
 
-//function validateAge(oldEnough){
-//  let logic = true;
-//  if (logic){
-//    document.getElementById("result").innerHTML = 'old enough';
-//function validateAge(oldEnough) {
-//  if (oldEnough === true) {
-//    return "old enough";
-
-
-//the index.js file
-  //    ✕ should create a function named `priceCheck` that takes a price and returns `match` if the price is equivalent to `1.50` (340 ms)
-  //    ✕ should return `match` when the string `1.50` is passed in as an argument to `priceCheck` (124 ms)
-  //    ✓ should not return `match` when `1.00` is passed in as an argument to `priceCheck` (127 ms)
-  //    ✕ should create a function named `strictPriceCheck` that takes a price and returns `match` only if the price is a number that is strictly equivalent to `1.50` (128 ms)
-  //    ✕ should not return `match` when the string `1.50` is passed in as an argument to `strictPriceCheck` (129 ms)
-  //    ✕ should not return `match` when `1.00` is passed in as an argument to `strictPriceCheck` (131 ms)
-  //
-//● the index.js file › should create a function named `priceCheck` that takes a price and returns `match` if the price is equivalent to `1.50`
-  
-  //    expect(received).toBe(expected) // Object.is equality
-  
-  //    Expected: "match"
-  //    Received: undefined
-  
-  //      46 |       });
-  //      47 |
-  //    > 48 |       expect(result).toBe('match');
-  //         |                      ^
-  //      49 |   });
-  //      50 |
-  //      51 |   it('should return `match` when the string `1.50` is passed in as an argument to `priceCheck`', async function() {
-  //
-  //      at Object.toBe (test/verify.test.js:48:22)
-  
-//● the index.js file › should return `match` when the string `1.50` is passed in as an argument to `priceCheck`
-  
-  //    expect(received).toBe(expected) // Object.is equality
-  
-  //    Expected: "match"
-  //    Received: undefined
-  //
-  //      54 |       });
-  //      55 |
-  //    > 56 |       expect(result).toBe('match');        
-  //         |                      ^
-  //      57 |   });
-  //      58 |
-  //      59 |   it('should not return `match` when `1.00` is passed in as an argument to `priceCheck`', async function() {
-  //
-  //      at Object.toBe (test/verify.test.js:56:22)
-  //
- 
-
-
-//should create a function named priceCheck 
-// that takes a price and returns the 
-// string match if the price is equivalent 
-// to 1.50
-
-//should return match when the string 1.50
-//  is passed in as an argument to
-//  priceCheck
-
-//should not return match when 1.00 is
-//  passed in as an argument to priceCheck
 
 //should create a function named 
 // strictPriceCheck that takes a price and 
